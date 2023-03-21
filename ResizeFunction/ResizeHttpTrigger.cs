bfb69db536b0068d3d3f5438d066cfe9ff62c90b
Jules Lalu <jules.lalu@gmail.com>
2023-03-21 15:19:25 +0100

    HTTP trigger

--- ResizeFunction/ResizeHttpTrigger.cs
+++ ResizeFunction/ResizeHttpTrigger.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ImageSharp;
 
 namespace ResizeleBleJ
 {
@@ -19,17 +20,36 @@ namespace ResizeleBleJ
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            int w = req.Query["w"];
+            int h = req.Query["h"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            byte[]  targetImageBytes;
+            using(var  msInput = new MemoryStream())
+            {
+                // Récupère le corps du message en mémoire
+                await req.Body.CopyToAsync(msInput);
+                msInput.Position = 0;
+
+                // Charge l'image       
+                using (var image = Image.Load(msInput)) 
+                {
+                    // Effectue la transformation
+                    image.Mutate(x => x.Resize(w, h));
+
+                    // Sauvegarde en mémoire               
+                    using (var msOutput = new MemoryStream())
+                    {
+                        image.SaveAsJpeg(msOutput);
+                        targetImageBytes = msOutput.ToArray();
+                    }
+                }
+            }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new FileContentResult(targetImageBytes, "image/jpeg");
         }
     }
 }
